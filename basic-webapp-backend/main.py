a76d9b473990279832a0f22f3c2bbcb8789be0d2
psyks17 <shroffkrish140@gmail.com>
2024-07-17 22:45:31 +0530

    changeeesd

--- basic-webapp-backend/main.py
+++ basic-webapp-backend/main.py
@@ -21,7 +21,7 @@ origins = [
     "http://127.0.0.1:5500",  # Your frontend origin
     "http://localhost:5500",
     "https://essay-llm-finetuner.vercel.app",   # Sometimes localhost is used
-    "https://essay-llm-finetuner.vercel.app",
+    "http://essay-llm-finetuner.vercel.app",
     "http://127.0.0.1:8000"
 ]
 
